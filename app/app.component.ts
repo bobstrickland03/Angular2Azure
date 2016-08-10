a9e64ec5e8efa7f0d9d45b52feedd3faba630f6a
Robert Strickland <robert.strickland@pearson.com>
2016-08-10 12:58:22 -0600

    Getting files from local using .gitignore from Angular2 quickstart

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>This is a test of hosting NODE Angular 2 app in Azure</h1>'
+})
+export class AppComponent { }
